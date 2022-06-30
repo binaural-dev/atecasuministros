cf51c6e94d40669bb0f0dc7f5c576cf18a3812bf
Jose <joseangel@binauraldev.com>
2022-06-30 15:31:12 -0400

    hotfix: "Eliminar modulo de Binaural contacto configuraciones"

--- modules/binaural_contactos_configuraciones/models/account_journal_inh.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class AccountJournalBinauralContactos(models.Model):
-    _inherit = 'account.journal'
-
-    journal_contingence = fields.Boolean(default=False, string="Contingencia")
